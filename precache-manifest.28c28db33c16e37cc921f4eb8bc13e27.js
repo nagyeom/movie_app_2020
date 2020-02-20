5462aad334e55dc82aa9468a0a043541fe6a4156
gyeom <khellantz@gmail.com>
2020-02-20 14:30:29 +0900

    Updates

---
+++ precache-manifest.28c28db33c16e37cc921f4eb8bc13e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58f68c93275d14a28f2c3ed283e796e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "63d7e873ec622aec54b9",
+    "url": "/movie_app_2020/static/css/main.d76376dd.chunk.css"
+  },
+  {
+    "revision": "39b8bcc7d2d72670fac8",
+    "url": "/movie_app_2020/static/js/2.39be1832.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2020/static/js/2.39be1832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d7e873ec622aec54b9",
+    "url": "/movie_app_2020/static/js/main.c768bd4f.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
