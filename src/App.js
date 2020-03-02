668cd44e55580ed713094bf903b5ef5c427840a3
gyeom <khellantz@gmail.com>
2020-03-02 09:35:19 +0900

    github.io 연결까지

--- src/App.js
+++ src/App.js
@@ -1,53 +1,12 @@
-import React from 'react';
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import React from "react";
+import { hashRouter ,Route} from "react-router-dom";
+improt About from "./routes/About";
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-  }
-  state = {
-    isLoading: true,
-    movies: []
-  };
+function App(){
+  return <HashRouter>
+    <Route path="/about" component={About} />
+  </HashRouter>
 
-  getMovies = async()=>{
-    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading:false});
-  }
-  componentDidMount() {
-    this.getMovies();
-  }
-  render(){
-    const { isLoading, movies } =this.state;
-    return (
-      <div>
-       {isLoading 
-       ? ( <div className="loader">
-         <span className="loader__text">Loading...</span>
-
-       </div> 
-       ): (
-        <div className="movies">
-          {movies.map(movie => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              genres={movie.genres} />
-          ))
-
-          }
-        </div>
-        )}
-      </div>
-
-    );
-  }
 }
 
 export default App;
